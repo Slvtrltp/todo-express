fa6e7e5d47d42c83e5dfa411d385e275cd5f1d6c
Nomin Ganzorig <nominganzorig@Nomins-MacBook-Air-6.local>
2026-05-11 03:21:51 +0800

    kkk

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import fs from "fs";
 import { nanoid } from "nanoid";
+import cors from "cors";
 
 import todoRouter from "./routers/todo-router.js";
 import usersRouter from "./routers/users-router.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/todos", todoRouter);
 app.use("/api/auth", usersRouter);
